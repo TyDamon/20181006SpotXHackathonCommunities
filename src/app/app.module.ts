6e0be59fb1aa1dee2da6e17b511cbf439086e3e2
Tyler Damon <tdamon@emagispace.com>
2018-10-06 12:48:07 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterNewUserComponent } from './register-new-user/register-new-user.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '/', component: LoginComponent }
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterNewUserComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
